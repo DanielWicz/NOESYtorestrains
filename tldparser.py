27314b226197e1f1b5422dbc118c90680a57078f
DanielWicz <daniel.wiczew@gmail.com>
2020-05-05 08:20:55 +0200

    Updating force field for aminoacids and adding rotatory mark

--- tldparser.py
+++ tldparser.py
@@ -1,5 +1,6 @@
 import csv
 import re
+from itertools import product
 
 class TblFileMaker:
     """A class that's pruporse is to create tbl restrain files from assigned
@@ -33,7 +34,7 @@ class TblFileMaker:
     def __init__(self, noecsv_fname=None, restwrite_fname=None, sequence_fname=None):
         # The values are d, d_minus, d_plus range: (d - d_minus, d + d_plus)
         self.basedict = {'w': [4.0, 2.2, 1.1], 'm': [3.0, 1.2, 0.5], 's': [2.5, 0.7, 0.4]}
-        self.forcefield_types = {'ALA': ['HN', 'HA', 'HB1', 'HB2', 'HB3'],
+        self.forcefield_types = {'ALA': ['HN', 'HA', 'HB1rot', 'HB2rot', 'HB3rot'],
                                  'ARG': [],
                                  'ASN': [],
                                  'ASP': [],
@@ -42,19 +43,19 @@ class TblFileMaker:
                                  'GLN': [],
                                  'GLY': ['HN', 'HA1', 'HA2'],
                                  'HIS': [],
-                                 'ILE': ['HN', 'HG11', 'HG12', 'HG21', 'HG22', 'HG23', 'HD11', 'HD12', 'HD13'],
+                                 'ILE': ['HN', 'HA', 'HB', 'HG11', 'HG12', 'HG21rot', 'HG22rot', 'HG23rot', 'HD11rot', 'HD12rot', 'HD13rot'],
                                  'LEU': [],
-                                 'LYS': [],
+                                 'LYS': ['HN', 'HA', 'HB1', 'HB2', 'HG1', 'HG2', 'HD1', 'HD2', 'HE1', 'HE2', 'HZ1rot', 'HZ2rot', 'HZ3rot'],
                                  'MET': [],
                                  'PHE': [],
                                  'PRO': [],
                                  'SER': [],
                                  'THR': [],
-                                 'TRP': [],
-                                 'TYR': ['HN', 'HA', 'HB1', 'HB2'],
+                                 'TRP': ['HN', 'HA', 'HB1', 'HB2', 'HD1', 'HE1', 'HE3', 'HZ2', 'HZ3', 'HH2'],
+                                 'TYR': ['HN', 'HA', 'HB1', 'HB2', 'HD1', 'HD2', 'HE1', 'HE2', 'HH'],
                                  'VAL': [],
                                  'CPC': ['HN', 'HA', 'HB', 'HG11', 'HG12', 'HG21', 'HG22', 'HD1', 'HD2'],
-                                 'ACE': ['H1', 'H2', 'H3']}
+                                 'ACE': ['HA1rot', 'HA2rot', 'HA3rot']}
 
         self.basestring = 'assign (resid {0} and name {1})(resid {2} and name {3}) {4} {5} {6}'
         self.csv_regexp = '([A-Z]+)([0-9]+)\ *[\–\-]\ *([A-Z]+)([0-9]+)'
@@ -70,8 +71,8 @@ class TblFileMaker:
         with open(self.sequence_fname, 'r') as f:
             for line in f:
                 for aa in line.strip().split(' '):
-                    if any(aa == '', aa == ' ', aa is None):
-                        raise ValueError('The file does have a'+\ 
+                    if any((aa == '', aa == ' ', aa is None)):
+                        raise ValueError('The file does have a'+\
                                          'bad structure, check it')
                     self.aminoacid_sequence.append(aa)
 
@@ -93,11 +94,11 @@ class TblFileMaker:
                 #    group(3) is second atom name that is paired
                 #    with the first atom
                 #    d, d_min, d_plus were described previously.
-                self.parameters_to_save.append(matched.group(2),
+                self.parameters_to_save.append((int(matched.group(2)),
                                                matched.group(1),
-                                               matched.group(4),
+                                               int(matched.group(4)),
                                                matched.group(3),
-                                               d, d_min, d_plus)
+                                               float(d), float(d_min), float(d_plus)))
 
     def atom_to_forcefield(self, atom_name=None, aminoacid=None):
         """For given atom and residue name, returns all possible
@@ -109,9 +110,9 @@ class TblFileMaker:
                           the atom.
            Returns: List of atom names for corresponding aminoacid.
         """
-        pass
+        pass 
 
-    def distance_restrain_pairs(self, entry_pair=None, aminoacid=None):
+    def distance_restrain_pairs(self, entry_pair=None):
         """Returns list of distance restrains for given distance restrain entry.
            
            Arguments:
@@ -123,6 +124,8 @@ class TblFileMaker:
            Returns: List of distance restrains for the given distance entry.
         """
         pass
+
+
            
     def restrain_postprocess(self):
         """Post processes generated distance restrains, by
@@ -135,7 +138,7 @@ class TblFileMaker:
                None
            Returns: List of distance restrains.
         """
-       pass 
+        pass
 
     def save_tbl(self):
         with open(self.restwrite_fname, 'w') as f:
@@ -143,6 +146,7 @@ class TblFileMaker:
             f.write(whole_tbl)
 
 if __name__ == '__main__':
-    tblmaker = TblFileMaker(noecsv_fname='noe.csv', restwrite_fname='protein.tbl')
+    tblmaker = TblFileMaker(noecsv_fname='noe.csv', restwrite_fname='protein.tbl',
+                            sequence_fname='sequence.seq')
     tblmaker.read_noecsv()
-    tblmaker.save_tbl()
+    tblmaker.read_sequence()
